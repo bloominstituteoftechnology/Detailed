b56851b87614083f8d1b1ab461d3d124522c35ad
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2018-11-14 17:05:15 -0500

    update storyboard

---
+++ Detailed/Detailed/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  Detailed
+//
+//  Created by Julian A. Fordyce on 11/14/18.
+//  Copyright © 2018 Julian A. Fordyce. All rights reserved.
+//
+
+import Foundation
---
+++ Detailed/Detailed/TableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewController.swift
+//  Detailed
+//
+//  Created by Julian A. Fordyce on 11/14/18.
+//  Copyright © 2018 Julian A. Fordyce. All rights reserved.
+//
+
+import Foundation
