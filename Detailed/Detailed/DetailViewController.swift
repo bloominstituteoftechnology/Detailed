f4df2d3062614bdcc651e588a946191b73e40b1a
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-11-14 14:57:13 -0800

    connect outlets to their respective types
    The label in the main view wasn't used to make a connection yet, as per the instruction's omission

--- Detailed/Detailed/DetailViewController.swift
+++ Detailed/Detailed/DetailViewController.swift
@@ -2,5 +2,7 @@ import UIKit
 
 
 class DetailViewController: UIViewController {
+    @IBOutlet weak var talkingCharacterLabel: UILabel!
     
+    @IBOutlet weak var talkingCharacterImageView: UIImageView!
 }
--- Detailed/Detailed/TableViewCell.swift
+++ Detailed/Detailed/TableViewCell.swift
@@ -2,4 +2,6 @@ import UIKit
 
 class TableViewCell: UITableViewCell {
     
+    @IBOutlet weak var talkingCharacterImageView: UIImageView!
+    
 }
