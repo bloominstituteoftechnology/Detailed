27d9a475c87260d39821ffbd07c876059a238eb9
Frulwinn <frulwinn@gmail.com>
2018-11-14 13:56:55 -0800

    finished adding objects on IB and made connections

--- Detailed/Detailed/DetailViewController.swift
+++ Detailed/Detailed/DetailViewController.swift
@@ -2,4 +2,8 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var imageBig: UIImageView!
+    
 }
--- Detailed/Detailed/TableViewCell.swift
+++ Detailed/Detailed/TableViewCell.swift
@@ -2,4 +2,7 @@ import UIKit
 
 class TableViewCell: UITableViewCell{
     
+    @IBOutlet weak var imageAnimation: UIImageView!
+    
+    
 }
