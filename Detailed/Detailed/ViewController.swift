0e3cb388d7e6d1f0634c1d2c8caf31054130b490
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-11-14 13:23:56 -0800

    initial project setup

---
+++ Detailed/Detailed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Detailed
+//
+//  Created by Jaspal on 11/14/18.
+//  Copyright © 2018 Jaspal Suri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
