22c70403b4233060afaeb980a4dee5c8a7d52278
Vijay Das <vijaytdas@gmail.com>
2018-11-27 16:38:08 -0600

    set up project, add talking character atlas assets for Bureaucrat, Frog, Ranger, Vendor, and Woodbot

---
+++ iOS4-Detailed/iOS4-Detailed/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  iOS4-Detailed
+//
+//  Created by Vijay Das on 11/27/18.
+//  Copyright © 2018 Vijay Das. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
+
