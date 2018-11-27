bed74f2a98e64ca1239dcd62f0df5093fe20e65d
Sean Doyle <sdoylelambda@gmail.com>
2018-11-27 17:14:33 -0500

    Model, TableVC, TableViewCell and DetailViewController skeletons added

---
+++ 11.27-Detailed/11.27-Detailed/Model.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+class Model {
+    static let shared = Model()
+    private init() {}
+    let shared = Model()
+    
+    
+}
---
+++ 11.27-Detailed/11.27-Detailed/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  11.27-Detailed
+//
+//  Created by Lambda_School_Loaner_18 on 11/27/18.
+//  Copyright © 2018 Lambda_School_Loaner_18. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
