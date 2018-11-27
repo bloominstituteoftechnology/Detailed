5beccb3145fcbc47d8b8825b1e248ff0e970cd35
Stuart Sippel <stuartsippel@icloud.com>
2018-11-27 16:55:06 -0600

    add class files, model files, setup interface

---
+++ Talking Animals/Talking Animals/TableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewCell.swift
+//  Talking Animals
+//
+//  Created by Stuart on 11/27/18.
+//  Copyright © 2018 Stuart. All rights reserved.
+//
+
+import Foundation
---
+++ Talking Animals/Talking Animals/TableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewController.swift
+//  Talking Animals
+//
+//  Created by Stuart on 11/27/18.
+//  Copyright © 2018 Stuart. All rights reserved.
+//
+
+import Foundation
