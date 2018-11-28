d275f8f7412ba7d98a1b5378e6f7ec92a026bd38
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2018-11-27 23:09:31 -0600

    Built model and Detail View Controller functionalities with some bugs to still be fixed

--- Detailed Project/Detailed Project/Model.swift
+++ Detailed Project/Detailed Project/Model.swift
@@ -1,10 +1,64 @@
+import UIKit
 
-import Foundation
+extension Int {
+    // Get a string version of an Int that is padded to the count given
+    fileprivate func stringPadded(to count: Int) -> String {
+        var string = String(self)
+        while string.count < count { string = "0" + string }
+        return string
+    }
+}
 
 class Model {
-    // MARK: - Singleton
     static let shared = Model()
     private init() {}
     
+    enum Animation: String, CaseIterable {
+        case bureaucrat = "Talking Bureaucrat"
+        case frog = "Talking Frog"
+        case ranger = "Talking Ranger"
+        case vendor = "Talking Vendor"
+        case bot = "Talking Woodbot"
+    }
+    
+    private let cellCounts = [Animation.bureaucrat: 26, .frog: 62, .ranger: 86, .vendor: 36, .bot: 69]
+    
+    var characters: [Animation] {
+        return Animation.allCases
+    }
+    
+    func image(for character: Animation) -> UIImage {
+        
+        // Assign the instance of Animation (character)'s raw value to imageName
+        let imageName = "\(character.rawValue)000"
+        
+        // Assign the UIImage of the imageName to 'image' if it exists, if not, return
+        guard let image = UIImage(named: imageName) else { return UIImage() }
+        
+        // Return the 00 Image for the given character
+        return image
+    }
+    
+    func cells(for character: Animation) -> [UIImage] {
+        // Return an array of images for the given character here
+        
+        // Create an empty array
+        var imageArray: [UIImage] = []
+        
+        // Get the values according to the character key
+        guard let index = cellCounts.index(forKey: character) else { return [] }
+        let max = cellCounts.values[index]
+        
+        // FIXME: Append UIImages 0 + index to the array
+        for eachImage in 0 ..< max {
+            let imageName = "\(character.rawValue)000"
+            if let image = UIImage(named: imageName) {
+                imageArray.append(image)
+            }
+            
+        }
+        
+       
+    }
     
 }
--- Detailed Project/Detailed Project/TableViewController.swift
+++ Detailed Project/Detailed Project/TableViewController.swift
@@ -6,4 +6,33 @@ class TableViewController: UITableViewController {
     // Implement reuse identifier for each cell
     static let reuseIdentifier = "animationcell"
     
+    // Number of rows = number of characters
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    // Cell's contents
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // get a cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "animationcell", for: indexPath)
+        
+        // set up the cell with the model object that we have
+        cell.imageView?.image = Model.shared.image(for: Model.Animation.allCases[indexPath.row])
+        
+        // return the character back to the table view
+        return cell
+    }
+    
+    // Prepare for segue in order to pass information that the segue's destination will use to set up its views
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // fetch index path and view controller
+        guard
+            let destination = segue.destination as? DetailViewController,
+            let indexPath = tableView.indexPathForSelectedRow
+            else { return }
+        
+        // FIXME: Prepare-for-segue method that sets the character property for the detail view controller
+        let character = Model.shared. // TODO: Fix this!
+    }
 }
