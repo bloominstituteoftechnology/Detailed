3a97680f5f001a5ac7bcca2a16105a25a0949e80
Meera Andersen <meera.andersen@gmail.com>
2018-11-27 17:14:59 -0700

    adding functionality to model

--- Detailed Week 2 Project/Detailed Week 2 Project/DetailViewController.swift
+++ Detailed Week 2 Project/Detailed Week 2 Project/DetailViewController.swift
@@ -2,4 +2,16 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    @IBOutlet weak var imageLabel: UILabel!
+    @IBOutlet weak var largeAnimation: UIImageView!
+    
+    var character = Character?
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        imageLabel.text =
+        largeAnimation.
+        
+    }
 }
--- Detailed Week 2 Project/Detailed Week 2 Project/TableViewCell.swift
+++ Detailed Week 2 Project/Detailed Week 2 Project/TableViewCell.swift
@@ -3,4 +3,11 @@ import SpriteKit
 
 class TableViewCell: UITableViewCell {
     
+    @IBOutlet weak var animationImage: UIImageView!
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+            return Model.shared.numberOfPeople()
+        }
+    }
 }
