931250542ae7e0c5314cf00a32c7af75389af34c
Stuart Sippel <stuartsippel@icloud.com>
2018-12-04 17:22:26 -0600

    build model

--- Detailed/Detailed/Model.swift
+++ Detailed/Detailed/Model.swift
@@ -29,9 +29,22 @@ class Model {
     // TODO: Implement
     func image(for character: Animation) -> UIImage {
         // RETURN THE 000 IMAGE FOR THE GIVEN CHARACTER HERE
+        let imageName = "\(character)000"
+        print(imageName)
+        guard let image = UIImage(named: imageName) else {fatalError("Image did not load.(Model.image)")}
+        return image
     }
     
     func cells(for character: Animation) -> [UIImage] {
         // RETURN AN ARRAY OF IMAGES FOR THE GIVEN CHARACTER HERE
+        let coalesceImage = UIImage(named: "\(character.rawValue)000")
+        var imageArray : [UIImage] = []
+        guard let count = cellCounts[character] else {fatalError("Error")}
+        for num in (0...count) {
+            let imageName = "\(character.rawValue)\(num.stringPadded(to: 3))"
+            imageArray.append((UIImage(named: imageName) ?? coalesceImage!))
+        }
+        return imageArray
     }
+
 }
