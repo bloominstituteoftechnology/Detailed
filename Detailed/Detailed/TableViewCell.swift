b35cdc12c7ddd7e06632b859e13ff6e55b7d364b
Vijay Das <vijaytdas@gmail.com>
2018-12-04 16:36:37 -0600

    first commit with basic project and skeleton class files for tableview, detailview and cell

---
+++ Detailed/Detailed/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Detailed
+//
+//  Created by Vijay Das on 12/4/18.
+//  Copyright © 2018 Vijay Das. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
