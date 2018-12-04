c5aca028fba081910115b5bdacb87c2340c59870
Sameera Leola <sameeraleola@gmail.com>
2018-12-04 15:52:54 -0600

    Setup the table view controller and the cell

---
+++ Detailed/Detailed/DetailViewController.swift
@@ -0,0 +1,12 @@
+//
+//  DetailViewController.swift
+//  Sprint 2 Review project - Detailed
+//
+//  Created by Sameera Leola on 12/4/18.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+}
---
+++ Detailed/Detailed/Model.swift
@@ -0,0 +1,8 @@
+//
+//  DetailViewController.swift
+//  Sprint 2 Review project - Detailed
+//
+//  Created by Sameera Leola on 12/4/18.
+//
+
+import UIKit
